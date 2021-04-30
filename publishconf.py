46da999ec60ab694127ce0970c04d5ebe82fc422
johnmathews <mthwsjc@gmail.com>
2021-04-30 12:45:17 +0200

    ignore output dir and cache

--- publishconf.py
+++ publishconf.py
@@ -6,8 +6,10 @@
 
 import os
 import sys
+
+from pelicanconf import *  # noqa
+
 sys.path.append(os.curdir)
-from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'http://atlascomputingservices.com'
@@ -17,8 +19,3 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
